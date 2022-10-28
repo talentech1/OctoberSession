09fd82afd9c21c2b2fd3c3bcdc25139a487397f9
Mohammed Siddique <arup2387+tt@gmail.com>
2022-10-27 20:24:16 -0400

    added review code

--- src/lesson/four/Laptop.java
+++ src/lesson/four/Laptop.java
@@ -10,7 +10,6 @@ public class Laptop {
 	private boolean isON = false;
 
 	private long onTime;
-	
 
 	public Laptop(String brand, String color, double price, int screenSize, int weight) {
 		this.brand = brand;
@@ -19,37 +18,42 @@ public class Laptop {
 		this.weight = weight;
 		this.screenSize = screenSize;
 	}
-	
+
 	public void turnOn() {
 		this.isON = true;
 		this.onTime = System.currentTimeMillis();
 	}
-	
+
 	public void turnOff() {
+		this.batteryPercentage = calculateCurrentBatteryLevel();
 		this.isON = false;
 		this.onTime = 0;
-		this.batteryPercentage = calculateCurrentBatteryLevel();
 	}
 
 	public String toString() {
 		return brand + ", " + color + ", " + price + ", " + screenSize + ", " + weight + ", ";
 	}
-	
+
 	public int calculateCurrentBatteryLevel() {
-		if(isON) {
-			long onDurationInMS =  System.currentTimeMillis() - this.onTime;
-			batteryPercentage = (int) (batteryPercentage - (onDurationInMS/100));
+		if (isON) {
+			long onDurationInMS = System.currentTimeMillis() - this.onTime;
+			batteryPercentage = (int) (batteryPercentage - (onDurationInMS / 100));
+			if (batteryPercentage <= 0) {
+				batteryPercentage = 0;
+				isON = false;
+			}
 			return batteryPercentage;
-		}else {
+		} else {
 			return batteryPercentage;
 		}
 	}
 
 	public void printStatus() {
+		int currentPercentage = calculateCurrentBatteryLevel();
 		if (isON) {
-			System.out.println("The laptop is on with battery " + calculateCurrentBatteryLevel());
-		}else {
-			System.out.println("The laptop is off with battery " + calculateCurrentBatteryLevel());
+			System.out.println("The laptop is on with battery " + currentPercentage);
+		} else {
+			System.out.println("The laptop is off with battery " + currentPercentage);
 		}
 
 	}
