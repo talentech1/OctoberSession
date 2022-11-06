9e7d134f764cfafb69558c8bf8856651170f2ab1
Mohammed Siddique <arup2387+tt@gmail.com>
2022-11-06 14:04:02 -0500

    testng

---
+++ src/review/five/TestNgExampleTwo.java
@@ -0,0 +1,23 @@
+package review.five;
+
+import org.testng.annotations.Test;
+
+public class TestNgExampleTwo {
+	
+	@Test(priority =3)
+	public void loginTest() {
+		
+	}
+	
+	@Test(priority =2)
+	public void logoutTest() {
+		
+	}
+	
+	@Test(priority =1)
+	public void signupTest() {
+		
+	}
+	
+	
+}
